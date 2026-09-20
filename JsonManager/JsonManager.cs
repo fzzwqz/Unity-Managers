08b00a618ee9e5aa1dc0631be72fbcdadb0fdb88
fzzwqz <2994592789@qq.com>
2026-09-20 09:31:38 +0800

    Initial commit: 添加 JsonManager 管理类和 README

---
+++ JsonManager/JsonManager.cs
@@ -0,0 +1,53 @@
+using LitJson;
+using System.IO;
+using UnityEngine;
+
+public enum JsonType 
+{
+    JsonUtility,
+    LitJson,
+}
+
+public class JsonManager 
+{
+    private static JsonManager instance=new JsonManager();
+    public static JsonManager Instance => instance;
+    private JsonManager() { }
+    public void SaveData(object data,string fileName,JsonType jsonType = JsonType.LitJson)
+    {
+        string path = Application.persistentDataPath + "/" + fileName + ".json";
+        string jsonStr="";
+        switch (jsonType)
+        {
+            case JsonType.LitJson:
+                jsonStr=JsonMapper.ToJson(data);
+                break;
+            case JsonType.JsonUtility:
+                jsonStr=JsonUtility.ToJson(data);
+                break;
+        }
+        File.WriteAllText(path, jsonStr);
+    }
+    public T LoadData<T>(string fileName, JsonType jsonType = JsonType.LitJson)where T : new()
+    {
+        string path = Application.persistentDataPath + "/" + fileName + ".json";
+        
+        if (!File.Exists(path))
+            path = Application.streamingAssetsPath + "/" + fileName + ".json";
+        if (!File.Exists(path))
+            return new T();
+
+        string  jsonStr =File.ReadAllText(path);
+        T data=default(T);
+        switch (jsonType)
+        {
+            case JsonType.LitJson:
+                data = JsonMapper.ToObject < T>(jsonStr);
+                break;
+            case JsonType.JsonUtility:
+                data=JsonUtility.FromJson< T>(jsonStr);
+                break;
+        }
+        return data;
+    }
+}
